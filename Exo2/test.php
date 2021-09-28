808b49aa960d24a423953b81e6580fab5b912ad0
V4lent1K <62839634+V4lent1K@users.noreply.github.com>
2021-09-28 14:35:41 +0200

    master

---
+++ Exo2/test.php
@@ -0,0 +1,10 @@
+<?php 
+    include("common/header.php");
+    include("common/menu.php");
+?>
+
+<!-- Content HTML -->
+
+<?php 
+    include("common/footer.php");
+?>
---
+++ Exo3/common/menu.php
@@ -0,0 +1,7 @@
+<nav>
+    <ul>
+        <li><a href="index.php">Accueil</a></li>
+        <li><a href="exo1.php">Exercice 1</a></li>
+        <li><a href="exo2.php">Exercice 2</a></li>
+    </ul>
+</nav>
